3cdbebe0a317463378cd696f692f085aefea2ea0
mv <michvett.mv@gmail.com>
2014-11-07 10:43:53 +0100

    add sepolicy dir
    Signed-off-by: mv <michvett.mv@gmail.com>

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,16 @@
+#
+# This policy configuration will be used by all products that
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/mv/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    fs_use \
+    genfs_contexts \
+    installd.te \
+    seapp_contexts \
+    vold.te \
+    mac_permissions.xml
